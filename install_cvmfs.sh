b3b115c32cd86a21eb153ae65d614e21658f9c87
root <root@evf-web.to.infn.it>
2017-11-08 15:44:28 +0100

    Added CMS specific config.

--- install_cvmfs.sh
+++ install_cvmfs.sh
@@ -13,6 +13,15 @@ CVMFS_CACHE_BASE=/var/lib/cvmfs
 CVMFS_HTTP_PROXY="http://t2-squid-01.to.infn.it:3128|http://t2-squid-02.to.infn.it:3128"
 EOF
 
+# specific for cms
+mkdir -p /etc/cvmfs/CMS_SITECONF/local/JobConfig/
+cp /cvmfs/cms.cern.ch/SITECONF/T1_IT_CNAF/JobConfig/site-local-config.xml /etc/cvmfs/CMS_SITECONF/local/JobConfig/
+
+echo "export CMS_LOCAL_SITE=T1_IT_CNAF" > /etc/cvmfs/config.d/cms.cern.ch.local
+
+cp /etc/cvmfs/config.d/cms.cern.ch.local /etc/cvmfs/config.d/cms.cern.ch.conf
+
+# start the service
 service autofs restart
 
 cvmfs_config setup
